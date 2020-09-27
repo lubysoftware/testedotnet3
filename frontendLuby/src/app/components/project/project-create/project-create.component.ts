61f96e1cd9c63ff69006355414e10ac50556a257
tvasconcelos90 <tiagobiker1@gmail.com>
2020-09-27 13:48:13 -0300

    Adding firt part of luby frontend project

---
+++ frontendLuby/src/app/components/project/project-create/project-create.component.ts
@@ -0,0 +1,35 @@
+import { ProductService } from './../../product/product.service';
+import { ProjectService } from './../project.service';
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../project.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-project-create',
+  templateUrl: './project-create.component.html',
+  styleUrls: ['./project-create.component.css']
+})
+export class ProjectCreateComponent implements OnInit {
+
+  project: Project = {
+    projectName: '',
+    projectDescription: ''
+  }
+
+  constructor(private projectService: ProjectService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  createProject(): void {
+    this.projectService.create(this.project).subscribe(() => {
+      this.projectService.showMessage("Produto criado!");
+      this.router.navigate(["/projects"]);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(["/projects"]);
+  }
+
+}
